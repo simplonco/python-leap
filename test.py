f7a60f2f2f5af5108f0f916857aaf5b2e72b4f6d
adora84 <abdulkader.kalaaji@gmail.com>
2016-06-02 11:48:06 +0200

    adora test

---
+++ test.py
@@ -0,0 +1,7 @@
+1	def leapyear(n):
+2	    if n%4==0 and n%100!=0:
+3	        if n%400==0:
+4	            print n, "it is a leap year."
+5	    elif n%4!=0:
+6	        print n, "it is not a leap year."
+7	print leapyear(1900)
